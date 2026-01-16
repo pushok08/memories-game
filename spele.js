a8c2f58a34645cec2276aa4bc4b5a6ef5110f330
pushok08 <erubike1@anip.lv>
2026-01-16 09:10:16 +0200

    16_01_26

--- spele.js
+++ spele.js
@@ -1,3 +1,23 @@
 //no URL iegūst vārdu
 let adrese = window.location.hash.substring(1);
-adrese = decodeURI(adrese.split(',')[0]);
+adrese = decodeURI(adrese.split(',')[0]|| '').trim();
+
+// mainīgie spēles darbībai
+let laiks = 0; //sekundes
+let kliski = 0;
+
+//taimers mainīgie (taimeris strādās ar pitmo klikski)
+let timerId = null;
+let timerstarted = false;
+
+
+
+
+
+
+const laukumiSaturs = ['🤩','🫨','🎃','🤡','🫨','💩','🥶','🤩','💩','🎃','🤡','🥶'];
+let atvertilaukumi = [];
+let pedejieDivi = [];
+
+//sajauc emoji nejaušā secībā
+let laukumiSajaukti = [...laukumiSaturs].sort(() => Math.random()) 
